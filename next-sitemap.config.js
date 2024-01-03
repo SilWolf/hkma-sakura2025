1668f83f8ebe6dc2e315c1b2914f5e5d2493025d
dicky.c <dicky.c@binance.com>
2024-01-04 04:05:10 +0800

    feat: add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://hkleague2024.hkmahjong.org/",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
