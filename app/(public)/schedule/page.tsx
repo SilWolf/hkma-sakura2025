2e38594d0eb8cfa6830fcae6a2a39a824b26ac3b
dicky.c <dicky.c@binance.com>
2024-12-30 01:30:44 +0800

    fix: tsc issues for Promise params

--- app/(public)/schedule/page.tsx
+++ app/(public)/schedule/page.tsx
@@ -4,11 +4,11 @@ export const revalidate = 900;
 
 export default async function ScheduleDefaultPage() {
   const date = new Date();
-  const params = {
+  const params = Promise.resolve({
     stage: "regulars" as const,
     year: date.getFullYear().toString(),
     month: (date.getMonth() + 1).toString(),
-  };
+  });
 
   return <SchedulePage params={params} />;
 }
